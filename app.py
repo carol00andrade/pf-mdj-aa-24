c4bb175d27273fa06a58fdda689d835e87392362
Ana Carolina Andrade <144967701+carol00andrade@users.noreply.github.com>
2024-04-10 10:53:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+# -*- coding: utf-8 -*-
 
+from flask import Flask, request, render_template
+import requests
+import os
+
+app = Flask(__name__)
+
+## Primeira parte do trabalho
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+    
+@app.route("/portfolio")
+def portifolio ():
+    return render_template("portfolio.html")
+
+@app.route("/curriculo")
+def curriculo ():
+    return render_template("curriculo.html")
