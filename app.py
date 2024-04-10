9ef3fe7ce57f2418e472b3b1abc588409bcb3c43
Ana Carolina Andrade <144967701+carol00andrade@users.noreply.github.com>
2024-04-10 15:14:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,26 +25,17 @@ def curriculo ():
 
 @app.route("/noticias")
 def noticias():
-    # Obter a palavra-chave da consulta da URL
     palavra_chave = request.args.get('palavra_chave', '')
-
-    # Se nenhuma palavra-chave for fornecida, exibir todas as notícias
     if not palavra_chave:
         return "Por favor, especifique uma palavra-chave na URL."
-    
     url = 'https://noticias.uol.com.br/'
-    
     palavras_chave_disponiveis = ['afogada', 'agredida', 'agrediu', 'agressão', 'amante', 'arma', 'assassinada', 'assassinadas', 'assassinato', 'assassinou', 'atirou', 'casamento', 'chuta', 'companheiro', 'consentimento', 'corpo', 'deputada', 'esfaqueada', 'esposa', 'estupro', 'ex-marido', 'ex-namorado', 'faca', 'facada', 'feminicídio', 'filha', 'gênero', 'machismo', 'machista', 'matar', 'marido', 'mataram', 'matava', 'matou', 'matamos', 'menina', 'morta', 'mortas', 'mortes', 'morre', 'morreu', 'mordida', 'mulher', 'namorada', 'queimada', 'sexual', 'tapa', 'tiro', 'vítima', 'violência', 'vereadora']
-    
     if palavra_chave not in palavras_chave_disponiveis:
         return "Palavra-chave inválida. Por favor, escolha uma das palavras-chave disponíveis."
-    
     manchetes_selecionadas = raspar_e_selecionar_noticias(url, [palavra_chave])
-        
     noticias_html = manchetes_selecionadas.to_html(index=False)
-
     return render_template("noticias.html", noticias_html=noticias_html)
-
+    
 def raspar_e_selecionar_noticias(url, palavras_chave):
     manchetes = raspar_noticias(url)
     manchetes_selecionadas = selecionar_noticias_com_palavra_chave(manchetes, palavras_chave)
@@ -72,8 +63,9 @@ def selecionar_noticias_com_palavra_chave(noticias, palavras_chave):
         for palavra in palavras_chave:
             if palavra in titulo.lower():
                 titulos_selecionados.append([titulo, link])
-                break  # Interrompe o loop assim que uma palavra-chave for encontrada
+                break  
     return titulos_selecionados
 
 if __name__ == "__main__":
     app.run(debug=True)
+
